8b3f2658858f3792f2c4423f4a15e4e39fafb782
Perry Neal <pneal20@ford.com>
2020-04-20 14:51:03 -0400

    Initial Commit

---
+++ App Catalog Flow Test/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  App Catalog Flow Test
+//
+//  Created by Neal, Perry (P.) on 4/20/20.
+//  Copyright © 2020 Neal, Perry (P.). All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
